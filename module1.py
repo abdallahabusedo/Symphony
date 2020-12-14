11c98d9c50936e2011acd5832e810232b2a570f3
abdallah abusedo <abozoooz2014.aaGemil.com>
2020-12-14 15:33:41 +0200

    local thresholding

--- module1.py
+++ module1.py
@@ -1,20 +1,20 @@
 import cv2
 from skimage.filters import gaussian ,threshold_otsu, threshold_local
-from skimage import data, io
+from skimage import io
 import numpy as np
-path = "note2.png"
-img = cv2.imread(path, 0)  # to read the image in a grey scale mode
-img_with_gaussian_filtered = gaussian(img,sigma=8);
-#cv2.cv2.imwrite('filtered.png', img_with_gaussian_filtered*255)
+Image_path = "note2.png"
+# to read the image in a grey scale mode
+Image = cv2.imread(Image_path, 0)
 
-# handle poop lightning: compare each pixel in the original image by the corresponding pixel in the filtered image
+# handle poop lightning: by applying local thresholding on the image
 # in order to apply thresholding
 # calculating the block size
+# ------------------------to do make it general to all images------------------------#
 block_size = 41
 # calculate the local threshold value
-threshold_local_value  = threshold_local(img ,block_size, offset=10)
+threshold_local_value  = threshold_local(Image ,block_size, offset=10)
 # apply the local threshold value on the image
-binary_image = img > threshold_local_value
+binary_image = Image > threshold_local_value
 
 # show the images
 io.imshow(binary_image)
