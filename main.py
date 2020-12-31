cc4f12285c62c855eba49e789612805dbed01c90
ayaadelhassan <ayaadelhassandarweesh@gmail.com>
2020-12-31 13:21:47 +0200

    rotate is final

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 
+#the main for one image
+
+import cv2
+from skimage import io
+import os
+import numpy as np
+from skimage.color import rgb2gray
+from skimage.measure import find_contours
+from transformation import *
+
+Original_image = io.imread('26.jpg')
+
+#rotating the image (if it needs rotation)
+Rotate_image = our_rotate(Original_image)
+# io.imshow(Rotate_image)
+# io.show()
 
 
 
@@ -10,17 +26,12 @@
 
 
 
-
-
-
-
-
-image = io.imread('26.jpg',as_gray="true")
-binary_image = Local_Thresholding(image)
-res = getFourCorners(binary_image)
-#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#result = divide(gray)
-#result3 = lineRemover(result[0])
-#result = objectDetection(result)
-io.imshow(res)
-io.show()
+# image = io.imread('26.jpg',as_gray="true")
+# binary_image = Local_Thresholding(image)
+# res = getFourCorners(binary_image)
+# #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# #result = divide(gray)
+# #result3 = lineRemover(result[0])
+# #result = objectDetection(result)
+# io.imshow(res)
+# io.show()
--- transformation.py
+++ transformation.py
@@ -8,19 +8,51 @@ import imutils
 
 
 
-def getAngel(img):
-    img = 255 - img
-    tested_angle = np.linspace(-np.pi / 2, np.pi / 2, 360)
-    h, theta, d = hough_line(img, theta=tested_angle)
-    ind = np.unravel_index(np.argmax(h, axis=None), h.shape)
-    degree = np.rad2deg(theta[ind[1]])
-    return degree
+def getAngle(img):
+    # Convert the image to gray-scale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Find the edges in the image using canny detector
+    edges = cv2.Canny(gray, 50, 200)
+    # Detect points that form a line
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 200, minLineLength=10, maxLineGap=250)
+    # Draw lines on the image
+    angles = []
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        # cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+        dx = x2 - x1
+        dy = y2 - y1
+        angles.append(np.rad2deg(np.math.atan2(dy, dx)))
 
+    angles = [angle for angle in angles if angle >= 0]
+    average_angle = sum(angles) / len(angles)
+    return average_angle
 
-image = io.imread('2.png', as_gray="true")
-bi = Local_Thresholding(image)
-angle = getAngel(bi)
-print(angle)
-rot = imutils.rotate(image, angle=angle)
-io.imshow(rot)
-io.show()
+def rotate_image(mat, angle):
+    height, width = mat.shape[:2] # image shape has 3 dimensions
+    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+
+    # rotation calculates the cos and sin, taking absolutes of those.
+    abs_cos = abs(rotation_mat[0,0])
+    abs_sin = abs(rotation_mat[0,1])
+
+    # find the new width and height bounds
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+    # rotate image with the new bounds and translated rotation matrix
+    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
+
+# the first call
+def our_rotate(RGBImage):
+    angle = getAngle(RGBImage)
+    rot = rotate_image(RGBImage,angle)
+    return rot
+
