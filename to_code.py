48c8098d6b0c8cc4239e19373234dab10a1ee5bc
Areej99 <areejraafat99@gmail.com>
2021-01-09 13:37:47 +0200

    symbols converted to code

---
+++ to_code.py
@@ -0,0 +1,173 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from os import listdir
+from skimage import io
+import glob
+import itertools
+code_line=['[']
+forlders_guide={'a_1': 'a/1', 'a_2': 'a/2', 'a_4': 'a/4','a_8':'a/8' , 'a_8d_8':'a/8 d/8' , 
+'a_16':'a/16','a_32':'a/32','a1_8b1_8':'a1/8 b1/8' , 'b_2':'b/2' , 'b_4':'b/4','clef':'',
+'c1_8g1_8':'c1/8 g1/8' , 'cdef1_16': 'c1/16 d1/16 e1/16 f1/16' , 'cgde1_16':'c1/16 g1/16 d1/16 e1/16',
+'cgde2_16':'c2/16 g2/16 d2/16 e2/16','4e_16': 'e1/16 e1/16 e1/16 e1/16', 'e_8c_8':'e/8 c/8',
+'fgab1_16':'f1/16 g1/16 a1/16 b1/16','b14_e14_g14':'{b/14 e/14 g/14}','b14_f14_g14' : '{b/14 f/14 g/14}',
+'c14_e14_g14':'{c/14 e/14 g/14}' ,'g14_e14':'{g/14 e/14}','#':'#','##':'##','&':'&','&&':'&&','.hi':'.',
+'2':'2','4':'4'
+ } 
+        # retval, img_binary = cv2.threshold(Image,235,255,type=cv2.THRESH_BINARY)
+        # dilation2 = cv2.dilate(img_binary,kernel,iterations =50)
+        # rect2 = cv2.getStructuringElement(cv2.MORPH_RECT, (60, 50)) #the structure element
+        # img_closing = binary_closing(dilation2,rect2)
+        # contours2 = find_contours(dilation2,0.8)
+        # results2 = []
+
+        # for c in contours2 :
+        #     ll2, ur2 = np.min(c, 0), np.max(c, 0) #getting the two points
+        #     wh2 = ur2 - ll2  #getting the width and the height
+        #     x2,y2,w2,h2 = ll2[0], ll2[1], wh2[1], wh2[0]
+        #     results2.append(x2) 
+        # imgs_lines.append(results2)
+
+ def arrange_code_string(code_line):
+    i=-1
+    for c in code_line :
+        i+=1
+        if i==2 :
+            if c=='4':
+            code_line[2]=code_line[2].replace('4','meter <"4/')
+            continue
+            elif c=='2':
+                code_line[2]=code_line[2].replace('2','meter <"2/')
+                continue
+        elif i==3 :
+            if c=='4':
+            code_line[3]=code_line[3].replace('4','4">')
+            continue
+            elif c=='2':
+                code_line[3]=code_line[3].replace('2','2">')
+                continue
+        if c=='#' or c=='##' or c=='&' or c=='&&':
+            symbol=c
+            code_line[i]=''
+            code_old=code_line[i+1]
+            code_new=code_old.split('/')[0] + symbol + '/' +code_old.split('/')[1] 
+            code_line[i+1]=code_new
+
+    code_string= " ".join(code_line)
+    return code_string
+
+def to_code(start,end,lines,folder_name):
+    code=""
+    code=forlders_guide.get(folder_name)
+    space=lines[1]=lines[0]
+    if code ==  'a/1' or code ==  'a/2' or code ==  'a/4' or code ==  'a/8' or code ==  'a/16' or code ==  'a/32':
+        if end <= lines[4]+ 2*space and end >= lines[4]+space :
+            code=code.replace('a','c1')
+        elif end <= lines[4]+space and end >= lines[4]:
+            code=code.replace('a','d1')
+        elif end <= lines[4]+0.5*space and end >= lines[3]+0.5*space:
+            code=code.replace('a','e1')
+        elif end <= lines[4] and end >= lines[3]:
+            code=code.replace('a','f1')
+        elif end <= lines[3]+0.5*space and end >= lines[2]+0.5*space:
+            code=code.replace('a','g1')
+        elif end <= lines[3] and end >= lines[2]:
+            code=code.replace('a','a1')
+    #--------------------------------------------------------------------------------------------------        
+    if code ==  'b/1' or code ==  'b/2' or code ==  'b/4' or code ==  'b/8' or code ==  'b/16' or code ==  'b/32':
+        if start <= lines[0] - 2*space :
+            code=code.replace('b','b2')
+        elif start<= lines[0]- 1.5*space and start >= lines[0]- 2*space :
+            code=code.replace('b','a2')
+        elif start<= lines[0] and start >= lines[0]-space :
+            code=code.replace('b','g2')
+        elif start<= lines[0]  and start >= lines[0]-0.5*space :
+            code=code.replace('b','f2')
+        elif start<= lines[1]  and start >= lines[0] :
+            code=code.replace('b','e2')
+        elif start<= lines[2]-0.5*space  and start > lines[1]-0.5*space :
+            code=code.replace('b','d2')
+        elif start<= lines[2]  and start >= lines[1] :
+            code=code.replace('b','c2')
+        elif start<= lines[3]-0.5*space  and start >= lines[2]-0.5*space :
+            code=code.replace('b','b1')
+        
+
+    return code
+
+def get_similar_temp(template):
+    folders = glob.glob("D:/college/cmp3/first/ip/symphony/Symphony/ourdata/*")
+    imagenames_list = [] #path to img
+    folder_name =[]
+    for folder in folders:
+        for f in glob.glob(folder+'/*'):
+                imagenames_list.append(f)
+                g = f.split('\\')
+                l = len(g)
+                folder_name.append(g[l - 2])
+                print(g[l-2])
+                #print (g)
+    read_images = []
+    for image in imagenames_list:
+        read_images.append(io.imread(image))
+    max_sim=0
+    max_path=""
+    max_img=[]
+    max_loc=0
+    for i, j in zip(read_images,folder_name):
+        # show_images([i],titles=[j])
+        print(j)
+        print(i.shape)
+        if len(i.shape) != 2:
+            img=cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+        else:
+            img=i
+        # cv2.imshow("i",i)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+        h, w = template.shape[::]
+        if h <= img.shape[0] and w <= img.shape[1]:
+            res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+            plt.imshow(res, cmap='gray')
+            threshold = 1 #Pick only values above 0.8. For TM_CCOEFF_NORMED, larger values = good fit.
+            maxx=[]
+            for r in res:
+                maxx.append(max(r))
+            print(max(maxx)*100)
+            if (max(maxx)*100 >= max_sim):
+                max_sim=max(maxx)*100
+                g = f.split('\\')
+                l = len(g)
+                max_path=g[l-2]
+                max_img=i
+                max_loc=np.where(res == max(maxx))
+            loc = np.where(res == max(maxx))
+            print(loc)  
+            #Outputs 2 arrays. Combine these arrays to get x,y coordinates - take x from one array and y from the other.
+            #Reminder: ZIP function is an iterator of tuples where first item in each iterator is paired together,
+            #then the second item and then third, etc. 
+        else:
+            print("no")
+    print("max_sim: ", max_sim)
+    print("path: ", max_path)
+    print("max_loc: ",max_loc)
+    for pt in zip(*max_loc[::-1]):   #-1 to swap the values as we assign x and y coordinate to draw the rectangle. 
+                #Draw rectangle around each object. We know the top left (pt), draw rectangle to match the size of the template image.
+        cv2.rectangle(max_img, pt, (pt[0] + w, pt[1] + h), (0, 0, 0), 1)
+        cv2.imshow("Matched image", max_img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+    return max_path
+img_to_compare = cv2.imread("D:/college/cmp3/first/ip/symphony/Symphony/a_8.2r-25.png",0)
+path=get_similar_temp(img_to_compare)
+
+code = to_code(12,30,[13,17,21,25,29],path)
+code_string=arrange_code_string(code)
+# print(code)
+
+
+
+# code_line.append(code)
+i=-1
+code_line2=['[','','4','4','#','e/16']
+print(code_line2)
