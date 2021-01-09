77e6413b0def8c660e9bac076bb7efd2360fb99c
abdallahabusedo <abozoooz2014.aa@gmail.com>
2021-01-09 14:55:24 +0200

    start merge

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ from transformation import *
 from thresholding import *
 from ReadWrite import *
 from preprocessing import *
+from to_code import *
 import time
 
 ##############################
 start = time.time()
 
-Original_image = io.imread(r'inputdata/test 2/10.PNG')
+Original_image = io.imread(r'inputdata/test 2/02.PNG')
 
 ##to modify the light in the image
 b = -30.  # brightness
@@ -27,6 +28,7 @@ thresholed_rotated = thresholding(Rotate_image)
 _, line_positions, Rows_images = divide(np.uint8(thresholed_rotated))
 
 removedImages = []
+biglineArray = []
 for row in Rows_images:
     bwArray = np.array(row)
     picWidth = len(bwArray[0])
@@ -34,6 +36,7 @@ for row in Rows_images:
     lineArray = getLines(horzPicCount, picWidth)
     lineThickness, newLineArray = findBarLineWidth(lineArray)
     lineArray = newLineArray
+    biglineArray.append(lineArray)
     spaceSize, spaceBetweenBars = findSpacesSize(lineArray, lineThickness)
     removed_line_pic = removeMe(row, lineArray, lineThickness)
     removedImages.append(removed_line_pic)
@@ -51,8 +54,15 @@ k = 0
 objectImages = []
 num = []
 objectImageswithlines = []
+# to be sent to shaza w areej
+final_array = []
+final_positions = []
 for j in range(0, len(removedImages)):
     objectDetectionImg, results = objectDetection(removedImages[j])
+    results = sorted(results)
+    objectImages = []
+    print(results)
+    final_positions.append(results)
     for box in results:
         X, Y, width, height = box
         if ymin <= Y + (height / 2) <= ymax:
@@ -64,11 +74,33 @@ for j in range(0, len(removedImages)):
                 objectImages.append(symbol)
                 objectImageswithlines.append(symbolline)
                 Image.fromarray(symbol).save(out_path + str(i) + ".png")
-                Image.fromarray(symbolline).save(out_path + str(i+1) + ".png")
+                Image.fromarray(symbolline).save(out_path + str(i + 1) + ".png")
             i += 2
-            print(symbol.shape[0], symbol.shape[1])
+    final_array.append(objectImages)
     num.append(len(finalobject))
     objectDetectionImages.append(objectDetectionImg)
 
+rowIndex = 0
+finallllllll = []
+for i in range(len(final_array)):  # 0 -> 3
+    code_line = ['[']
+    ob = final_array[i]
+    #print("ob", len(ob))
+    objectIndex = 0
+    for xc in ob:
+        #print("Asd",final_positions[objectIndex][0],final_positions[objectIndex][0] + final_positions[objectIndex][3] ,biglineArray[i])
+        path = get_similar_temp(xc)
+        #print("print" , path)
+        code = to_code(final_positions[i][objectIndex][0],
+                       final_positions[i][objectIndex][0] + final_positions[i][objectIndex][3],
+                       biglineArray[i], path)
+        code_line.append(code)
+        objectIndex += 1
+    code_line.append(']')
+    code_string = arrange_code_string(code)
+    finallllllll.append(code_string)
+finallllllll = "\n".join(finallllllll)
+
+print(finallllllll)
 end = time.time()
 print(f"Runtime of the program is {end - start}")
